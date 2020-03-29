f120d88d0908f1aab7c475f806098a9e66640ae3
Stanislav <31693185+Azazelloo@users.noreply.github.com>
2020-03-29 18:31:19 +0300

    Add files via upload

--- testDLLclient/testDLLclient/Header.h
+++ testDLLclient/testDLLclient/Header.h
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <windows.h>
 #include <iomanip>
+#include <wchar.h>
 
 using namespace std;
 
@@ -15,16 +16,23 @@ static SC_HANDLE schSCManager;
 
 struct ServicesObj
 {
-	WCHAR* Name;
-	unsigned int PID=0;
-	const WCHAR* DispName;
-	unsigned int state=0;
-	const WCHAR* group;
-	const WCHAR* path;	
+	WCHAR* m_name;
+	unsigned int m_pid=0;
+	const WCHAR* m_dispName;
+	unsigned int m_state=0;
+	WCHAR* m_group;
+	WCHAR* m_path;	
+public:
+	ServicesObj()
+	{
+		m_path=new WCHAR[128]();
+		m_group=new WCHAR[128]();
+	}
 };
 
 MYDLL_API int GetServicesList(ServicesObj* s);
-MYDLL_API void GetCountServices(DWORD& size);
+MYDLL_API int GetCountServices(DWORD& size);
 MYDLL_API int StopSrv(WCHAR* name);
 MYDLL_API int StartSrv(WCHAR* name);
 MYDLL_API int RestartSrv(WCHAR* name);
+MYDLL_API void MyWcsncpy(WCHAR* s_to,const WCHAR* s_from,size_t count);
--- testDLLclient/testDLLclient/main.cpp
+++ testDLLclient/testDLLclient/main.cpp
@@ -1,6 +1,4 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
-
-#include "Header.h"
+﻿#include "Header.h"
 #include <iostream>
 #include <cstring>
 using namespace std;
@@ -8,20 +6,20 @@ using namespace std;
 
 int main()
 {
-	DWORD size = 0;
-	int err;
-	GetCountServices(size);
-	ServicesObj* ss=new ServicesObj[size]; //добавить функцию считающую количество служб
+	DWORD size=0;
+	int err=1;
+	err=GetCountServices(size);
+	ServicesObj* ss=new ServicesObj[size](); //добавить функцию считающую количество служб
 	GetServicesList(ss);
 
 	for (size_t i=0;i<size;i++)
 	{
 		cout << "[" << i << "] = ";
-		wcout<<ss[i].Name << endl;
-	}
+		wcout<<ss[i].m_name <<"->"<<ss[i].m_pid<<"->"<<ss[i].m_group<< endl;
+	}	
 
 	//err=StopSrv(ss[0].Name); //остановка службы
 	//err=StartSrv(ss[0].Name);// запуск службы
-	err=RestartSrv(ss[0].Name);
-	return 0;
+	//err=RestartSrv(ss[0].Name);
+ 	return 0;
 }
