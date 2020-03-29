94ab1bc519faf99532d2fe7e355ac31435d74562
Stanislav <31693185+Azazelloo@users.noreply.github.com>
2020-03-29 18:29:26 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+﻿#include "Header.h"
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+int main()
+{
+	DWORD size=0;
+	int err=1;
+	err=GetCountServices(size);
+	ServicesObj* ss=new ServicesObj[size](); //добавить функцию считающую количество служб
+	GetServicesList(ss);
+
+	for (size_t i=0;i<size;i++)
+	{
+		cout << "[" << i << "] = ";
+		wcout<<ss[i].m_name <<"->"<<ss[i].m_pid<<"->"<<ss[i].m_group<< endl;
+	}	
+
+	//err=StopSrv(ss[0].Name); //остановка службы
+	//err=StartSrv(ss[0].Name);// запуск службы
+	//err=RestartSrv(ss[0].Name);
+ 	return 0;
+}
