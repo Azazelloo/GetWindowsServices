08bc7a5292cdd43d7cbad7a1add8d8806ea5e488
Stanislav <31693185+Azazelloo@users.noreply.github.com>
2020-03-30 17:54:44 +0300

    Add files via upload

--- testDLLclient/testDLLclient/Header.h
+++ testDLLclient/testDLLclient/Header.h
@@ -9,6 +9,8 @@
 #include <windows.h>
 #include <iomanip>
 #include <wchar.h>
+#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -16,23 +18,15 @@ static SC_HANDLE schSCManager;
 
 struct ServicesObj
 {
-	WCHAR* m_name;
+	wstring m_name;
 	unsigned int m_pid=0;
-	const WCHAR* m_dispName;
+	wstring m_dispName;
 	unsigned int m_state=0;
-	WCHAR* m_group;
-	WCHAR* m_path;	
-public:
-	ServicesObj()
-	{
-		m_path=new WCHAR[128]();
-		m_group=new WCHAR[128]();
-	}
+	wstring  m_group;
+	wstring  m_path;	
 };
 
-MYDLL_API int GetServicesList(ServicesObj* s);
-MYDLL_API int GetCountServices(DWORD& size);
+MYDLL_API vector<ServicesObj> GetServicesList();
 MYDLL_API int StopSrv(WCHAR* name);
 MYDLL_API int StartSrv(WCHAR* name);
 MYDLL_API int RestartSrv(WCHAR* name);
-MYDLL_API void MyWcsncpy(WCHAR* s_to,const WCHAR* s_from,size_t count);
